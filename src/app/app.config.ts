56e5aaf11d6b01fda4cfe1e225eb6279f9ed5e59
randyduong08 <71861208+randyduong08@users.noreply.github.com>
2024-03-24 18:58:10 -0400

    Pwa installation (#1)
    * Installed angular/pwa
    
    - Updated Angular
    - Installed latest Angular/pwa
    
    * Finished installing PWA
    
    - Installed Angular SSR
    - Finished installing PWA
    - Added npm scripts for convenience
    
    * Installed tailwind
    
    Installed tailwind css (to support potential CSS libraries/components like Aceternity)
    
    * Added spartan/ui to project
    
    Installed various stuff to allow for usage of spartan UI library (spartan.ng)
    
    * added spartan library
    
    * Implemented NavBar
    
    - Implemented NavBar with example of spartan ui usage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
-    })]
+    }), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    }), provideClientHydration()]
 };
