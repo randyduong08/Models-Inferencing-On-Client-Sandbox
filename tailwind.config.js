fee0deeb399a72e1aa9bfaa2f38df35c3facc57b
randyduong08 <randyduong08@gmail.com>
2024-03-24 18:47:50 -0400

    added spartan library

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
   content: [
     "./src/**/*.{html,ts}",
-    "./src/app/components/**/*.{html,ts}"
+    "./src/app/components/**/*.{html,ts}",
+    "./libs/ui/**/*.{html,ts}"
   ],
   theme: {
     extend: {},
