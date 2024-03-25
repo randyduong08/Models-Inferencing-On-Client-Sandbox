e2484a06c47a4c19721e1e62719dc987935aa101
randyduong08 <randyduong08@gmail.com>
2024-03-25 00:32:21 -0400

    Added navbar
    Added navbar For top-middle of screen

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
   imports: [
+    MatMenuModule,
+    RouterModule
   ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss',
 })
 export class NavBarComponent {
+  isDropdownOpen: boolean = false;
 
+  @HostListener('mousehover')
+  openDropdown(): void {
+    this.isDropdownOpen = true;
+  }
+
+  @HostListener('mouseleave')
+  closeDropdown(): void {
+    this.isDropdownOpen = false;
+  }
 }
