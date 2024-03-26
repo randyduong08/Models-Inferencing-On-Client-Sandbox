393618c7c162bb89c0c4ad40285537b2f6d1c07c
Duong, Randy <RandyDuong@rocketinnovationstudio.ca>
2024-03-26 13:27:42 -0400

    onnx runtime files
    - added onnx runtime files

--- src/app/components/sentiment/sentiment.component.ts
+++ src/app/components/sentiment/sentiment.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
 import * as onnx from 'onnxruntime-web';
+import { AutoTokenizer } from '@xenova/transformers';
 
 @Component({
   selector: 'app-sentiment',
@@ -11,9 +12,17 @@ import * as onnx from 'onnxruntime-web';
 })
 export class SentimentComponent implements OnInit, OnDestroy {
   session!: onnx.InferenceSession;
+  options: any;
 
   async ngOnInit(): Promise<void> {
-    
+    onnx.env.wasm.wasmPaths = "https://cdn.jsdelivr.net/npm/onnxruntime-web/dist/";
+
+    this.options = {
+      executionProviders: ['wasm'],
+    };
+
+    this.session = await onnx.InferenceSession.create('../../../assets/quant_xtreme_distil_sentiment.onnx', this.options);
+
   }
 
   ngOnDestroy(): void {
