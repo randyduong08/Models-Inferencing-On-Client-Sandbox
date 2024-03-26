ffa778d80417467012ea6fb597c87f8bddba7a05
randyduong08 <randyduong08@gmail.com>
2024-03-25 23:04:21 -0400

    Sentiment page - pt 1
    - Added sentiment model to be used in the assets folder
    - Added textfield to sentiments page, to be used for grabbing text to analysis sentiment on

--- src/app/components/sentiment/sentiment.component.ts
+++ src/app/components/sentiment/sentiment.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
+import * as onnx from 'onnxruntime-web';
 
 @Component({
   selector: 'app-sentiment',
   standalone: true,
-  imports: [],
+  imports: [HlmInputDirective],
   templateUrl: './sentiment.component.html',
   styleUrl: './sentiment.component.scss'
 })
-export class SentimentComponent {
+export class SentimentComponent implements OnInit, OnDestroy {
+  session!: onnx.InferenceSession;
 
+  async ngOnInit(): Promise<void> {
+    
+  }
+
+  ngOnDestroy(): void {
+      
+  }
 }
